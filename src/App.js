349afbe88e65e65115d4cc7432af69366310d9bf
sushilv3 <sushilv3rma@gmail.com>
2021-05-15 16:36:31 +0530

    handle multiple input values with single useState hook

--- src/App.js
+++ src/App.js
@@ -3,38 +3,54 @@ import './App.css';
 
 function App() {
 
-const[name, setName] = useState();
-const[newName, setNewName] = useState();
-
-const[lastName,setLastName] = useState();
-const[newLastName,setNewLastName] = useState();
+const [fullName, setFullName] = useState({
+  fname:'',
+  lname:'',
+});
 
   const inputEvent = (event)=>{
     console.log(event.target.value);
-    setName(event.target.value);
-  };
+    console.log(event.target.name);
+    console.log(event.target.placeholder);
 
+    const value = event.target.value;
+    const name = event.target.name;
 
-  const inputEventTwo = (event)=>{
-    setLastName(event.target.value);
+    setFullName((preValue)=>{
+      console.log("@@@@@@@@@@@@@@", preValue)
+
+      // name is present input attribute name
+
+      if(name === "fName"){
+        return{
+          fname: value,
+          lname:preValue.lname
+        };
+      }else if(name === "lName"){
+        return{
+          fname: preValue.fname,
+          lname:value
+        };
+      }
+    });
   };
 
   const onSubmit = (event) =>{
     event.preventDefault();
-
-    setNewName(name);
-    setNewLastName(lastName);
+    alert('form submitted');
   };
+
   return (
     <>
     <div className="main_div">
       <form onSubmit={onSubmit} >
           <div>
-            <h1>Hello, {newName} {newLastName}</h1>
-            <input type="text" placeholder="Enter Your Name" onChange={inputEvent}  value={name} />
+            <h1>Hello, {fullName.fname} {fullName.lname}</h1>
+            <input type="text" name="fName" placeholder="Enter Your Name" onChange={inputEvent} value={fullName.fname} />
             <br />
 
-            <input type="text" placeholder="Enter Your Last Name" onChange={inputEventTwo}  value={lastName} />
+            <input type="text" name="lName" placeholder="Enter Your Last Name" onChange={inputEvent}  value={fullName.lname} />
+
             <button type="submit" >Click Me</button>
           </div>
       </form>
