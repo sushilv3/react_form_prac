20dd2880483954c14b519cd605a884e07fe50dc0
sushilv3 <sushilv3rma@gmail.com>
2021-05-15 09:28:11 +0530

    controlled component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+const[name, setName] = useState();
+const[fullName, setFullName] = useState();
+
+  const inputEven = (event)=>{
+    console.log(event.target.value);
+    setName(event.target.value)
+  };
+
+  const onSubmit = () =>{
+    setFullName(name);
+  }
   return (
-    <h1>hello</h1>
+    <>
+      <div>
+         <h1>Hello {fullName}</h1>
+         <input type="text" placeholder="Enter Your Name" onChange={inputEven}  value={name}/>
+         <button onClick={onSubmit} >Click Me</button>
+      </div>
+    </>
   );
 }
 
